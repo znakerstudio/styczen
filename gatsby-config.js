a95045d03d57fe52a7468fca6be2dc00d4fe85f9
znakerstudio <znakerstudio@gmail.com>
2022-01-18 18:42:29 +0100

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,10 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Fotogra`,
+    siteTitleAlt: `Fotogra Starter Portfolio`,
+    siteHeadline: `Fotogra`,
+    siteUrl: `https://fotogra.netlify.app/`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
